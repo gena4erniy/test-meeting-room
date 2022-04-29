1e587fc3e6c807fad3f55c2ac29618f738567950
s.kudaev <s.kudaev@andersenlab.com>
2022-04-29 11:57:45 +0300

    ADD Test Case 5628568 "User autorization using valid e-mail and invalid password"

---
+++ src/test/java/tests/ui/us11/AuthorInvalPass.java
@@ -0,0 +1,53 @@
+package tests.ui.us11;
+
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.TmsLink;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import ui.pages.AuthorizationPage;
+import ui.pages.LocationChoose;
+
+import java.net.MalformedURLException;
+
+import static ui.constants.Constant.InValidCredo.*;
+import static ui.constants.Constant.ValidCredo.VALID_MAIL;
+
+import static ui.pages.BasePage.capabilities;
+
+@Epic("Login")
+@Feature("US 1.1")
+
+public class AuthorInvalPass {
+    private AndroidDriver<AndroidElement> driver;
+    private AuthorizationPage authorizationPage;
+    private LocationChoose locationchoose;
+
+    @BeforeClass
+    private void preconditions() throws MalformedURLException {
+        driver = capabilities();
+        authorizationPage = new AuthorizationPage();
+        locationchoose = new LocationChoose();
+    }
+
+
+    @AfterClass
+    public void closeDriver() {driver.quit();}
+
+
+
+    @Owner("Kudayeu S.")
+    @TmsLink("5628568")
+    @Test(description = "User autorization using valid e-mail and invalid password")
+    public void authorizationInvalPass(){
+
+        driver.findElement(authorizationPage.getEmailInput()).sendKeys(VALID_MAIL);
+        driver.findElement(authorizationPage.getPasswordInput()).sendKeys(INVALID_PASS);
+        driver.findElement(authorizationPage.getButtonLogin()).click();
+        Assert.assertTrue(driver.findElement(authorizationPage.getAlert()).isDisplayed(), "test failed");
+    }
+}
