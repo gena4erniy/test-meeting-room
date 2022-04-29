5370deb157f83887a016d101ab1535c3e361c522
s.kudaev <s.kudaev@andersenlab.com>
2022-04-29 12:15:05 +0300

    ADD Test Case 5628125 "User authorization using empty fields"

---
+++ src/test/java/tests/ui/us11/AuthEmptyFields.java
@@ -0,0 +1,44 @@
+package tests.ui.us11;
+
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.TmsLink;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import ui.pages.AuthorizationPage;
+import ui.pages.LocationChoose;
+
+import java.net.MalformedURLException;
+import static ui.pages.BasePage.capabilities;
+
+@Epic("Login")
+@Feature("US 1.1")
+
+public class AuthEmptyFields {
+    private AndroidDriver<AndroidElement> driver;
+    private AuthorizationPage authorizationPage;
+
+    @BeforeClass
+    private void preconditions() throws MalformedURLException {
+        driver = capabilities();
+        authorizationPage = new AuthorizationPage();
+    }
+
+
+    @AfterClass
+    public void closeDriver() {driver.quit();}
+
+
+
+    @Owner("Kudayeu S.")
+    @TmsLink("5628125")
+    @Test(description = "User authorization using empty fields")
+    public void authorizationNull() {
+        driver.findElement(authorizationPage.getButtonLogin()).click();
+        Assert.assertFalse(driver.findElement(authorizationPage.getButtonLogin()).isEnabled(), "test failed");
+    }
+}
