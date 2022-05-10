f6168465fc15041ebe64fd7b475484f0696ed3a2
Krystsina <k.vaskovich@andersenlab.com>
2022-05-10 14:33:48 +0300

    refactor tests

--- src/test/java/tests/ui/us21/TapOnEventTitleUpcomingTest.java
+++ src/test/java/tests/ui/us21/TapOnEventTitleUpcomingTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 import tests.ui.BaseTest;
 import ui.pages.*;
 
-public class TapOnEventTitleUpcoming extends BaseTest {
+public class TapOnEventTitleUpcomingTest extends BaseTest {
     private LocationChoose locationchoose;
     private MySpacePage mySpacePage;
     private ModifyEventPage modifyEventPage;
@@ -19,14 +19,15 @@ public class TapOnEventTitleUpcoming extends BaseTest {
         locationchoose = new LocationChoose();
         mySpacePage = new MySpacePage();
         modifyEventPage = new ModifyEventPage();
+
+        driver.findElement(locationchoose.getNextButton()).click();
+        Assert.assertTrue(driver.findElement(mySpacePage.getUpcomingTab()).isSelected(), "The Upcoming control not displays in active mode by default.");
     }
 
     @Owner("Vaskovich K.")
     @TmsLink("5634863")
     @Test(description = "Verification that the Modify Event screen is displayed")
     public void TapOnEventTitleUpcomingTest() {
-        driver.findElement(locationchoose.getNextButton()).click();
-        Assert.assertTrue(driver.findElement(mySpacePage.getUpcomingTab()).isSelected(), "The Upcoming control not displays in active mode by default.");
         Assert.assertTrue(driver.findElement(mySpacePage.getEventTitle()).isDisplayed() && driver.findElement(mySpacePage.getEventTime()).isDisplayed() &&
                 driver.findElement(mySpacePage.getEventDate()).isDisplayed() && driver.findElement(mySpacePage.getEventRoom()).isDisplayed(), "Event title not contains: 'Event title', 'Event start-end time', 'Event date', 'Event room'.");
         driver.findElement(mySpacePage.getEventCard()).click();
