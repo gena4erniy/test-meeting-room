b6bdbe3524d69e3fb2ff9c009ddfccffb9c1e68d
e.sirozh <e.sirozh@andersenlab.com>
2022-05-11 14:50:06 +0300

    refactor getter

--- src/main/java/ui/pages/BelarusCitiesChoose.java
+++ src/main/java/ui/pages/BelarusCitiesChoose.java
@@ -7,4 +7,5 @@ import org.openqa.selenium.By;
 public class BelarusCitiesChoose {
     private final By buttonMinsk = By.xpath("//android.widget.TextView[@text = 'Minsk']");
     private final By buttonGomel = By.xpath("//android.widget.TextView[@text = 'Gomel']");
+    private final By buttonBack = By.id("com.andersenlab.meeting_room.debug:id/arrow_back_location_fragment");
 }
--- src/main/java/ui/pages/RoomsPage.java
+++ src/main/java/ui/pages/RoomsPage.java
@@ -1,27 +1,15 @@
 package ui.pages;
 
+import lombok.Getter;
 import org.openqa.selenium.By;
-
+@Getter
 public class RoomsPage {
    // private final By roomsTitle = By.xpath("//android.widget.FrameLayout[@content-desc='Rooms']/android.widget.ImageView");
     private final By nextDate = By.id("com.andersenlab.meeting_room.debug:id/next");
     private final By choseDate = By.xpath("//android.widget.CheckedTextView[@content-desc='9']");
     private final By choseEvent = By.id("com.andersenlab.meeting_room.debug:id/eventView");
+    private final By choseDate13 = By.xpath("//android.widget.CheckedTextView[@content-desc='13']");
 
  private final By eventCard = By.id("com.andersenlab.meeting_room.debug:id/event_card_upcoming_root");
 
-    public By getEventCard() {
-        return eventCard;
-    }
-
-    public By getNextDate() {
-        return nextDate;
-    }
-
-    public By getChoseDate() {
-        return choseDate;
-    }
-    public By getChoseEvent(){
-        return choseEvent;
-    }
-}
+ }
