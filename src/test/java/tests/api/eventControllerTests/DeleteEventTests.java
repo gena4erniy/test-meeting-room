d0127417bdbe6d9c826727ed7ccdd7c1f5ba6da6
e.sirozh <e.sirozh@andersenlab.com>
2022-06-01 13:46:33 +0300

    f

--- src/test/java/tests/api/eventControllerTests/DeleteEventTests.java
+++ src/test/java/tests/api/eventControllerTests/DeleteEventTests.java
@@ -22,7 +22,6 @@ import static ui.constants.Constant.ValidCredo.VALID_PASS;
 
 public class DeleteEventTests {
     private MeetingRoomClient meetingRoomClient;
-    private JSONObject loginRequestDto;
     private JSONObject eventDto;
     private String startDateTime = "2022-05-27T17:53:47.439";
     private String endDateTime = "2022-05-27T19:53:47.439";
@@ -38,21 +37,8 @@ public class DeleteEventTests {
     @Test(description = "Get information create event")
     @Description("create event")
     public void getInfoDeleteEvent() {
-        JSONObject responseGetInfoAuthUser = meetingRoomClient.deleteCall("api/events/3569" , eventDto);
+        JSONObject responseGetInfoAuthUser = meetingRoomClient.deleteCall("api/events/3569", eventDto);
         Assertions.assertThat(responseGetInfoAuthUser.getInt("Status Code")).isEqualTo(204);
 
-//        SoftAssert asserts = new SoftAssert();
-//        asserts.assertEquals(responseGetInfoAuthUser.getInt("id"), 3569);
-//        asserts.assertEquals(responseGetInfoAuthUser.getString("title"), "red");
-//        assertThat(responseGetInfoAuthUser.get("room")).isNotNull();
-//        assertThat(responseGetInfoAuthUser.get("user")).isNotNull();
-//        asserts.assertAll();
     }
 }
-//{
-//        "id": 3589,
-//        "startDateTime": "2022-06-01T13:18:51.35",
-//        "endDateTime": "2022-06-01T14:18:51.35",
-//        "roomId": 1,
-//        "type": "REGULAR_EVENT"
-//        }
