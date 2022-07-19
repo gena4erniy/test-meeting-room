e7e895ae9a9f412e9c8b9eb331190469168a4139
Tetiana Tatarina <t.tatarina@andersenlab.com>
2022-07-19 11:02:38 +0300

    Create autotests AdminAuthorizationTests, AuthorizationInvalidTests, LogOutTests

--- src/test/java/tests/ui/login/us11/AuthorizationInvalidTests.java
+++ src/test/java/tests/ui/login/us11/AuthorizationInvalidTests.java
@@ -1,5 +1,4 @@
-package tests.ui.us11;
-
+package tests.ui.login.us11;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -12,39 +11,38 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ui.pages.AuthorizationPage;
-import ui.pages.LocationChoose;
 
 import java.net.MalformedURLException;
 
-import static ui.constants.Constant.ValidCredo.VALID_MAIL;
 import static ui.constants.Constant.ValidCredo.VALID_PASS;
 import static ui.pages.BasePage.capabilities;
 
 @Epic("Login")
 @Feature("US 1.1")
-public class AuthorizationTests {
+public class AuthorizationInvalidTests {
     private AndroidDriver<AndroidElement> driver;
-    private AuthorizationPage authorizationPage;
-    private LocationChoose locationchoose;
 
     @BeforeClass
     private void preconditions() throws MalformedURLException {
         driver = capabilities();
-        authorizationPage = new AuthorizationPage();
-        locationchoose = new LocationChoose();
-
     }
 
     @AfterClass
-    public void closeDriver() {driver.quit();}
+    public void closeDriver() {
+        driver.quit();
+    }
 
-    @Owner("Kudayeu S.")
-    @TmsLink("5628118")
-    @Test(description = "User authorization using valid data")
-    public void authorization(){
-        driver.findElement(authorizationPage.getEmailInput()).sendKeys(VALID_MAIL);
+    @Owner(value = "Tatarina T.")
+    @TmsLink("5628121")
+    @Test(description = "User authorization using missigned \"@\" e-mail")
+    public void authorizationMissingEmail() {
+        AuthorizationPage authorizationPage = new AuthorizationPage();
+        driver.findElement(authorizationPage.getEmailInput()).sendKeys("numb3andersenlab.com");
+        Assert.assertFalse(driver.findElement(authorizationPage.getButtonLogin()).isEnabled());
         driver.findElement(authorizationPage.getPasswordInput()).sendKeys(VALID_PASS);
+        Assert.assertTrue(driver.findElement(authorizationPage.getButtonLogin()).isEnabled());
         driver.findElement(authorizationPage.getButtonLogin()).click();
-        Assert.assertTrue(driver.findElement(locationchoose.getSelectLoc()).isDisplayed(), "Login is not succeed");
+        Assert.assertTrue(driver.findElement(authorizationPage.getAlert()).isDisplayed());
+        Assert.assertEquals(driver.findElement(authorizationPage.getAlert()).getText(), "Incorrect e-mail or password!");
     }
 }
